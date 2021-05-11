dd88a426b6c36a0d58a13d956af22938acb854f8
Charbel Naba <charbelnaba@gmail.com>
2021-05-11 13:00:26 +0200

    added new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+import { MoviesService } from './services/movies.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesTableComponent } from './movies-table/movies-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesTableComponent
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
@@ -15,7 +18,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
